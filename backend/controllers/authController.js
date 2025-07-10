51acd8e7f47fadde73fa4cf591b559465f32bb2d
Yuva Deekshitha Namani <yuvadeekshithanamani@gmail.com>
2025-07-10 18:38:53 +0530

    authcontroller update

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -29,8 +29,12 @@ exports.register = async (req, res) => {
 
     // Generate JWT token
     const token = jwt.sign(
-      { id: user._id, role: user.role },
-      JWT_SECRET,
+    {
+      email:user.email,
+      role:user.role,
+      name:ussername,
+    },
+      process.env.JWT_SECRET,
       { expiresIn: "1d" }
     );
 
